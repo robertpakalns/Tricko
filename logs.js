c360385c51c1cc7b01ce5da762bbc1156fdd0e82
robertpakalns <136381220+robertpakalns@users.noreply.github.com>
2024-07-28 18:17:59 +0300

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,6 +1,6 @@
 const blackList = ["@ss", "ass", "bitch", "b1tch", "bltch", "bastard", "cock", "c0ck", "cum", "cunt", "dick", "fuck", "nigga", "niga", "nigger", "niger", "pussy", "sex", "shit"]
-const whiteList = ["assault"]
-// the arrays will supplemented later
+const whiteList = ["assault", "lasso", "passion", "class", "massive", "solasso", "ericasso"]
+// arrays will be supplemented later
 
 const creationTime = t => `<t:${Math.ceil(new Date(t).getTime() / 1000)}:R>` // Discord relative date format
 
@@ -51,13 +51,25 @@ export const checkName = (name, game, mode, type) => {
 
 export const checkBrActivity = (name, array) => {
     if (typeof name !== "string") return
-    const potentialFarmMatches = array.filter(el => el.deaths !== 0 && // user has no deaths
-                                                    el.rank !== 1 && // user has not won the match
-                                                    el.survival_time < 60 // user played less than 60 seconds
-                                             ).length
-    if (potentialFarmMatches / array.length >= 0.7) sendWebhook(name, "Voxiom.io", "Player", "BR Gem Farming") // if 7 or more potentialFarmMatches, then it counts as gem farming (currently)
+    const potentialFarmMatches = array.filter(el =>
+        el.deaths !== 0 && // player has no deaths
+        el.rank !== 1 && // player has not won the match
+        el.survival_time < 60 && // player played less than 60 seconds
+        new Date(el.time) > new Date(Date.now() - 2500000000) // match wass earlier than ~30 days ago (relatively new)
+    ).length
+    if (potentialFarmMatches / array.length >= 0.7) sendWebhook(name, "Voxiom.io", "Player", "BR Gem Farming")
+}
+
+export const checkDuplicates = (name, array) => {
+    const map = array.reduce((acc, el) => {
+        if (el.item.name) acc[el.item.name] = (acc[el.item.name] || 0) + 1
+        return acc
+    }, {})
+    const isDupe = Object.values(map).some(i => i >= 2)
+    if (isDupe) sendWebhook(name, "Vectaria.io", "Player", "Skin Dupe")
 }
 
 // use
-// checkName("someBadWord", "Voxiom.io", "Player", "Name")
-// checkBrActivity("nickname", array) //Voxiom.io BR only
+// checkName(badName, "Voxiom.io", "Player", "Name")
+// checkBrActivity(name, array) //Voxiom.io BR only
+// checkDuplicates(name, array) //Vectaria.io only
