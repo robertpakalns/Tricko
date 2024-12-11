09b6ecdc9d7251d74f04913179aa980494754a06
robertpakalns <136381220+robertpakalns@users.noreply.github.com>
2024-12-11 21:04:34 +0200

    Update builder.js

--- builder.js
+++ builder.js
@@ -1,95 +1,114 @@
 export class Builder {
-    constructor(interaction = Object) {
+    constructor(interaction) {
         this.interaction = interaction
-        this.servicePromise = this.interaction.deferReply().then(() => Promise.resolve())
-        this.hasError = false
-        this.messageID = null
+        this.promise = this.interaction.deferReply()
     }
 
-    formatEmbed(embed = Object) {
+    formatEmbed(embed) {
         return {
             ...embed,
             color: embed.color || 16777215,
-            footer: { text: "Powered By Tricko", icon_url: "https://tricko.pro/icon.png" }
+            footer: { text: "Powered by Tricko", icon_url: "https://bot.tricko.pro/bot_icon.webp" }
         }
     }
 
-    setService(url = String, options = Object) {
-        this.servicePromise = this.servicePromise.then(async () => {
-            const response = await fetch(url, options)
-            const data = await response.json()
+    handleError(error) {
+        const { commandName = null, options = null } = this.interaction
+        const optionsMap = options?._hoistedOptions?.map(el => `${el.name} = ${el.value}`).join("|") || null
 
-            if (data.success === false) {
-                this.hasError = true
-                const { code, text, game, type } = data
-                const errorEmbed = {
-                    title: "ERROR",
-                    description: `Code: \`${code}\`\nText: \`${text}\`\nGame: \`${game}\`\nType: \`${type}\``,
-                    color: 16711680
+        this.setError(`An error occurred while executing \`${commandName}\` command with parameters \`${optionsMap}\`. The error has been logged. Please try again later.`)
+
+        fetch(webhook, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ content: `Builder error. Command name: \`/${commandName}\` Options: \`${optionsMap}\` \`\`\`${error.stack}\`\`\`` })
+        })
+    }
+
+    setService(path, options) {
+        this.promise = fetch(`https://api.tricko.pro${path}${path.includes("?") ? "&" : "?"}log=bot`, options)
+            .then(r => r.json())
+            .then(data => {
+                if (data.success === false) {
+                    const { code, text, game, type } = data
+                    this.setError(`Code: \`${code}\`\nText: \`${text}\`\nGame: \`${game}\`\nType: \`${type}\``)
+                    return null
                 }
-                await this.interaction.editReply({ embeds: [this.formatEmbed(errorEmbed)] })
-            }
-            else return data
-        })
+                return data
+            })
+        return this
+    }
+
+    do(callback) {
+        this.promise
+            .then(data => data !== null && callback(data, this))
+            .catch(error => this.handleError(error))
 
         return this
     }
 
-    do(callback = Function) {
-        this.servicePromise.then(data => { if (data != undefined) callback(data, this) })
-        return this
+    setMessage({ embeds = [], attachments = [], urls = [] }) {
+        this.promise.then(data => {
+            if (data === null) return
+
+            const formattedEmbeds = embeds.map(el => this.formatEmbed(el))
+            const maxPage = embeds.length
+
+            let page = 0
+            const setRow = () => ({
+                type: 1,
+                components: [...(embeds.length > 1 ? [
+                    { type: 2, custom_id: "back", label: "Back", style: 2, disabled: page === 0 },
+                    { type: 2, custom_id: "next", label: "Next", style: 2, disabled: page === maxPage - 1 }
+                ] : []),
+                ...urls.map(el => ({ type: 2, label: el.name, style: 5, url: el.value }))]
+            })
+
+            const updateMessage = () => this.interaction.editReply({
+                embeds: [formattedEmbeds[page]],
+                components: [setRow()],
+                files: attachments[page] ? [attachments[page]] : []
+            })
+
+            updateMessage().then(message => {
+                if (maxPage > 1) message.createMessageComponentCollector({ filter: i => i.user.id === this.interaction.user.id, time: 6000 })
+                    .on("collect", i => {
+                        page += i.customId === "next" ? 1 : i.customId === "back" ? -1 : 0
+                        updateMessage().then(() => i.deferUpdate())
+                    })
+                    .on("end", () => {
+                        const newRow = setRow()
+                        newRow.components = newRow.components.filter(el => el.style === 5)
+                        this.interaction.editReply({ components: [newRow] }).catch(() => { })
+                    })
+            })
+        }).catch(error => this.handleError(error))
     }
 
-    async setMessage({ embeds = Array, attachments = Array, urls = [] }) {
-        await this.servicePromise
-        if (this.hasError === true) return this
+    setError(description) {
+        this.promise.then(() => this.interaction.editReply({
+            embeds: [this.formatEmbed({ title: "ERROR", description, color: 16711680 })],
+            attachments: [],
+            files: []
+        }))
+    }
 
-        const formattedEmbeds = embeds.map(embed => this.formatEmbed(embed))
-
-        let page = 0
-        const maxPage = formattedEmbeds.length
-
-        const setRow = max => ({
-            type: 1,
-            components: (embeds.length > 1 ? [
-                { type: 2, custom_id: "back", label: "Back", style: 2, disabled: page === 0 },
-                { type: 2, custom_id: "next", label: "Next", style: 2, disabled: page === max - 1 },
-            ] : []).concat(urls.map(url => ({ type: 2, label: url.name, style: 5, url: url.value })))
-        })
-
-        const updateMessage = async page => await this.interaction.editReply({
-            embeds: [formattedEmbeds[page]],
-            components: setRow(maxPage).components.length > 0 ? [setRow(maxPage)] : [],
-            files: attachments[page] ? [attachments[page]] : []
-        })
-
-        this.messageID = (await updateMessage(page)).id
-
-        if (maxPage > 1) {
-            this.interaction.channel.createMessageComponentCollector({ filter: i => i.user.id === this.interaction.user.id && i.message.id === this.messageID, time: 60000 })
-                .on("collect", async i => {
-                    if (i.customId === "back") page--
-                    if (i.customId === "next") page++
-
-                    await updateMessage(page)
-                    i.deferUpdate()
-                })
-                .on("end", async () => {
-                    const newRow = setRow(maxPage)
-                    newRow.components.filter(i => i.style !== 5).forEach(i => i.disabled = true)
-                    await this.interaction.editReply({ components: [newRow] })
-                })
-        }
-
-        return this
+    setWarning(description) {
+        this.promise.then(() => this.interaction.editReply({
+            embeds: [this.formatEmbed({ title: "WARNING", description, color: 16777060 })],
+            attachments: [],
+            files: []
+        }))
     }
 }
 
-//use
+// Use
 // new Builder(interaction)
-//   .setService(url, options) //optional
-//   .do((data, builder) => { //in case setService was used and you have to work with fetched data
+//   .setService(url, options)                            // optional
+//   .do((data, builder) => {                             // in case setService was used and you have to work with fetched data
+//
 //     // your code goes here
+//
 //     builder.setMessage({
 //       embeds: [],
 //       attachments: [],
@@ -100,5 +119,5 @@ export class Builder {
 // * embeds
 // * attachments
 // * rows with buttons
-//   * if more than 1 embed in embeds, then two grey buttons (back and next) appear 
+//   * if more than 1 embed in embeds[], then two pagination buttons appear 
 //   * style buttons (optional)
